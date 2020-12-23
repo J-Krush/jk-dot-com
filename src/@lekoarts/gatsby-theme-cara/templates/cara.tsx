4d2a42c907f04e6ca941c2923d90c0b467abe5f2
John Kreisher <jkreish@gmail.com>
2020-12-22 21:45:48 -0500

    refactor: remove projects

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -14,13 +14,13 @@ const Cara = () => {
   
   return (
       <Layout>
-        <Parallax pages={7}>
+        <Parallax pages={5}>
           <Hero offset={0} factor={1} />
           <Stats offset={1} factor={1} />
           <Skills  offset={2} factor={1} />
-          <Projects offset={3} factor={2} />
-          <About offset={5} factor={1} />
-          <Contact offset={6} factor={1} />
+          {/* <Projects offset={3} factor={2} /> */}
+          <About offset={3} factor={1} />
+          <Contact offset={4} factor={1} />
         </Parallax>
       </Layout>
     )
