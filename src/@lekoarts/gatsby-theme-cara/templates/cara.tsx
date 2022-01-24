14e3dda9de40879d2a71a94595da551d52f211e8
John Kreisher <jkreish@gmail.com>
2022-01-24 13:08:22 -0500

    refactor: remove stats from index

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -3,11 +3,11 @@ import React, { useState, useEffect } from "react"
 import { Parallax } from "react-spring/renderprops-addons.cjs"
 import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout"
 import Hero from "@lekoarts/gatsby-theme-cara/src/components/hero"
-import Projects from "@lekoarts/gatsby-theme-cara/src/components/projects"
+// import Projects from "@lekoarts/gatsby-theme-cara/src/components/projects"
 import About from "@lekoarts/gatsby-theme-cara/src/components/about"
 import Contact from "../components/contact"
 import Skills from "../components/skills"
-import Stats from "../components/stats"
+// import Stats from "../components/stats"
 
 const Cara = () => {  
   return (
@@ -15,7 +15,7 @@ const Cara = () => {
         <Parallax pages={6}>
           <Hero offset={0} factor={1} />
           <Skills  offset={1} factor={1} />
-          <Stats offset={2} factor={2} />
+          {/* <Stats offset={2} factor={2} /> */}
           {/* <Projects offset={3} factor={2} /> */}
           <About offset={4} factor={1} />
           <Contact offset={5} factor={1} />
